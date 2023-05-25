7b7a2d770b244ffc88b1fb38fd489ddbc3a0284a
DhananjayP5 <pokharkardhananjay511@gmail.com>
2023-05-25 17:06:35 +0530

    homepage setup, postgres configration and .env-sample

--- FoodOnline_main/settings.py
+++ FoodOnline_main/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'FoodOnline_main.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'FoodOnline_main.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'foodOnline_db',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST' : 'localhost',  
+
     }
 }
 
@@ -116,6 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR /'static'
+STATICFILES_DIRS = [
+    'FoodOnline_main/static'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
